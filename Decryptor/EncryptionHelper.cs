a8eb447e518694e1417164577701912a2b7d72c9
ogugunava <ogugunava@dataart.com>
2022-06-22 16:08:29 +0400

    Add project files.

---
+++ Decryptor/EncryptionHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TTL.Encryption;
+
+namespace Decryptor
+{
+    internal static class EncryptionHelper
+    {
+        public static string Decrypt(string encryptedText, string encryptionKeyName)
+        {
+            var keyFile = string.Format("c:\\private\\{0}.key", encryptionKeyName);
+            return Crypto.Decrypt(encryptedText, File.ReadAllText(keyFile));
+        }
+    }
+}
---
+++ Decryptor/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Decryptor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please chose environment: for development type-d, production-p. to exit type - exit");
+                var option = Console.ReadLine();
+                if (option == "exit")
+                {
+                    break;
+                }
+
+                if (!(option == "d" || option == "p"))
+                {
+                    continue;
+                }
+
+                Console.WriteLine("Please paste encrypted string:");
+
+                var text = Console.ReadLine();
+
+                var result= EncryptionHelper.Decrypt(text, option == "d" ? "dev" : "prod_alt");
+
+                Console.WriteLine($"Decrypted value = {result}");
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+           
+        }
+    }
+}
